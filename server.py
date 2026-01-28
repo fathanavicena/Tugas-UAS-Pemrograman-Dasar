71de4ee8c5ed64d1e6f0f175361bce033e2db110
Fathan <fathanauddin@gmail.com>
2026-01-28 13:36:47 +0700

    Update server.py

--- server.py
+++ server.py
@@ -2,22 +2,36 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Data awal di Server (Bobot: 50)
+# Database sementara di sisi Server
 database_toko = [
-    {"kode": "L01", "nama": "Laptop Gaming", "harga": 15000000},
-    {"kode": "H02", "nama": "Smartphone S23", "harga": 12000000},
-    {"kode": "T03", "nama": "Smart TV 4K", "harga": 5000000}
+    {
+        "kode": "L01", 
+        "nama": "LAPTOP GAMING ASUS", 
+        "harga": 15000000, 
+        "kategori": "Laptop",
+        "foto": [] # Tempat menyimpan string Base64 foto
+    },
+    {
+        "kode": "H02", 
+        "nama": "SMARTPHONE S23", 
+        "harga": 12000000, 
+        "kategori": "Handphone",
+        "foto": []
+    }
 ]
 
 @app.route('/api/barang', methods=['GET'])
 def ambil_barang():
+    # Mengirim seluruh data ke Client
     return jsonify(database_toko), 200
 
 @app.route('/api/barang', methods=['POST'])
 def simpan_barang():
+    # Menerima data produk baru dari Client
     data_baru = request.json
     database_toko.append(data_baru)
     return jsonify({"pesan": "Berhasil disimpan di Server!"}), 201
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    # Berjalan di port 5000 (Default Flask)
+    app.run(port=5000, debug=True)
